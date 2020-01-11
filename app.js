69e627cad035cbf4bcaeff3b17060a759a79dde1
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2020-01-12 03:36:44 +0530

    news pwa

---
+++ app.js
@@ -0,0 +1,50 @@
+const apiKey = 'cf7f3c64d5b24083b7e9db822088fda1';
+const defaultSource = 'the-washington-post';
+const sourceSelector = document.querySelector('#sources');
+const newsArticles = document.querySelector('main');
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () =>
+    navigator.serviceWorker.register('sw.js')
+      .then(registration => console.log('Service Worker registered'))
+      .catch(err => 'SW registration failed'));
+}
+
+window.addEventListener('load', e => {
+  sourceSelector.addEventListener('change', evt => updateNews(evt.target.value));
+  updateNewsSources().then(() => {
+    sourceSelector.value = defaultSource;
+    updateNews();
+  });
+});
+
+window.addEventListener('online', () => updateNews(sourceSelector.value));
+
+async function updateNewsSources() {
+  const response = await fetch(`https://newsapi.org/v2/sources?apiKey=${apiKey}`);
+  const json = await response.json();
+  sourceSelector.innerHTML =
+    json.sources
+      .map(source => `<option value="${source.id}">${source.name}</option>`)
+      .join('\n');
+}
+
+async function updateNews(source = defaultSource) {
+  newsArticles.innerHTML = '';
+  const response = await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&sortBy=top&apiKey=${apiKey}`);
+  const json = await response.json();
+  newsArticles.innerHTML =
+    json.articles.map(createArticle).join('\n');
+}
+
+function createArticle(article) {
+  return `
+    <div class="article">
+      <a href="${article.url}">
+        <h2>${article.title}</h2>
+        <img src="${article.urlToImage}" alt="${article.title}">
+        <p>${article.description}</p>
+      </a>
+    </div>
+  `;
+}
---
+++ sw.js
@@ -0,0 +1,45 @@
+const cacheName = 'news-v1';
+
+const staticAssets = [
+  './',
+  './app.js',
+  './styles.css',
+  './fallback.json',
+  './images/fetch-dog.jpg'
+];
+
+self.addEventListener('install', async function () {
+  const cache = await caches.open(cacheName);
+  cache.addAll(staticAssets);
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  const url = new URL(request.url);
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(request));
+  } else {
+    event.respondWith(networkFirst(request));
+  }
+});
+
+async function cacheFirst(request) {
+  const cachedResponse = await caches.match(request);
+  return cachedResponse || fetch(request);
+}
+
+async function networkFirst(request) {
+  const dynamicCache = await caches.open('news-dynamic');
+  try {
+    const networkResponse = await fetch(request);
+    dynamicCache.put(request, networkResponse.clone());
+    return networkResponse;
+  } catch (err) {
+    const cachedResponse = await dynamicCache.match(request);
+    return cachedResponse || await caches.match('./fallback.json');
+  }
+}
